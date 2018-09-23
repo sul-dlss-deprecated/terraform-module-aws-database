1de14869591690b121a4d4f98f92d13f8a05159c
Aaron Collier <aaron.collier@stanford.edu>
2018-09-23 16:30:39 -0700

    Initial RDS module

---
+++ rds/main.tf
@@ -0,0 +1,31 @@
+resource "aws_db_parameter_group" "rds-param-group" {
+  name   = "${var.name}-rds-params"
+  family = "${var.family}"
+
+  parameter {
+    name  = "client_encoding"
+    value = "${var.encoding}"
+  }
+}
+
+resource "aws_db_instance" "rialto_db_instance" {
+  allocated_storage         = "${var.storage}"
+  storage_type              = "${var.storage_type}"
+  engine                    = "${var.engine}"
+  engine_version            = "${var.engine_version}"
+  instance_class            = "${var.instance_class}"
+  identifier                = "${var.identifier}"
+  name                      = "${var.name}"
+  username                  = "${var.username}"
+  password                  = "${var.password}"
+  parameter_group_name      = "${aws_deb_parameter_group.rds-param-group.name}"
+  skip_final_snapshot       = "${var.skip_final_snapshot}"
+  db_subnet_group_name      = "${var.subnet_group_name}"
+  backup_retention_period   = "${var.backup_retention_period}"
+  backup_window             = "${var.backup_window}"
+  final_snapshot_identifier = "${var.final_stopshot_identifer}"
+
+  tags {
+    Note = "Deployed by terraform"
+  }
+}
---
+++ rds/variables.tf
@@ -0,0 +1,38 @@
+variable name {}
+variable family {
+    default = "postgres9.5"
+}
+variable encoding {
+    default = "utf8"
+}
+
+variable storage {}
+variable storage_type {}
+variable engine {
+    default = "postgres"
+}
+variable engine_version {
+    default = "9.5"
+}
+variable instance_class {
+    default = "db.t2.medium"
+}
+
+variable identifer {}
+variable username {}
+variable password {}
+
+variable skip_final_snapshot {
+    default = false
+}
+
+variable subnet_group_name {}
+
+variable backup_retention_period {
+    default = 5
+}
+variable backup_window {
+    default = "22:00-23:00"
+}
+
+variable final_stopshot_identifer {}
