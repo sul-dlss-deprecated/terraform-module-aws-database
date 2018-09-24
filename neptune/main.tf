7042929dc9cee260a28bab8e2fd8454446e318ca
Aaron Collier <aaron.collier@stanford.edu>
2018-09-24 09:34:10 -0700

    Revert "Add param group for parity"
    This reverts commit 65660db816b190226d591503d24d16b0281c3c47.

--- neptune/main.tf
+++ neptune/main.tf
@@ -23,7 +23,6 @@ resource "aws_neptune_cluster" "neptune_cluster" {
   backup_retention_period             = "${var.backup_retention_period}"
   preferred_backup_window             = "${var.backup_window}"
   skip_final_snapshot                 = "${var.skip_final_snapshot}"
-  neptune_cluster_parameter_group_name = "${aws_neptune_parameter_group.neptune-param-group.name}"
   neptune_subnet_group_name           = "${aws_neptune_subnet_group.subnet_group.name}"
   iam_database_authentication_enabled = false
   apply_immediately                   = true
