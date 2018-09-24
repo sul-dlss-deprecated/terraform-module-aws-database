cdfa75adeaddf3ef34ff13811b9f72a01a2d2250
Aaron Collier <aaron.collier@stanford.edu>
2018-09-24 13:51:08 -0700

    Add from_snapshot

---
+++ neptune/from_snapshot/variables.tf
@@ -0,0 +1,34 @@
+variable name {}
+variable family {
+    default = "neptune1"
+}
+variable query_timeout {
+    default = "60"
+}
+
+variable engine {
+    default = "neptune"
+}
+variable engine_version {
+    default = "9.5"
+}
+variable instance_class {
+    default = "db.r4.large"
+}
+
+variable skip_final_snapshot {
+    default = false
+}
+
+variable backup_retention_period {
+    default = 5
+}
+variable backup_window {
+    default = "22:00-23:00"
+}
+
+variable subnet_ids {
+    type = "list"
+}
+
+variable snapshot_identifier {}
