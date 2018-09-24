7c1bf85096b3f0ebef33706ca680ee5ccefbc687
Aaron Collier <aaron.collier@stanford.edu>
2018-09-24 09:11:52 -0700

    Add subnet group name to cluster and instance

--- neptune/main.tf
+++ neptune/main.tf
@@ -23,6 +23,7 @@ resource "aws_neptune_cluster" "neptune_cluster" {
   backup_retention_period             = "${var.backup_retention_period}"
   preferred_backup_window             = "${var.backup_window}"
   skip_final_snapshot                 = "${var.skip_final_snapshot}"
+  neptune_subnet_group_name           = "${aws_neptune_subnet_group.subnet_group.name}"
   iam_database_authentication_enabled = false
   apply_immediately                   = true
 }
@@ -33,6 +34,7 @@ resource "aws_neptune_cluster_instance" "neptune_instance" {
   instance_class               = "${var.instance_class}"
   cluster_identifier           = "${var.name}-neptune-cluster"
   neptune_parameter_group_name = "${aws_neptune_parameter_group.neptune-param-group.name}"
+  neptune_subnet_group_name    = "${aws_neptune_subnet_group.subnet_group.name}"
   apply_immediately            = true
 
   tags {
