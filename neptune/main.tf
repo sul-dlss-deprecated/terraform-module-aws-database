75a2d862b7c718397c73bc358888bd7beb05677e
Aaron Collier <aaron.collier@stanford.edu>
2018-09-24 08:32:49 -0700

    Fix typo

--- neptune/main.tf
+++ neptune/main.tf
@@ -9,7 +9,7 @@ resource "aws_neptune_parameter_group" "neptune-param-group" {
 }
 
 resource "aws_neptune_subnet_group" "subnet_group" {
-  name = "${var.name}-subnet-group"
+  name       = "${var.name}-subnet-group"
   subnet_ids = ["${var.subnet_ids}"]
 
   tags {
@@ -18,13 +18,13 @@ resource "aws_neptune_subnet_group" "subnet_group" {
 }
 
 resource "aws_neptune_cluster" "neptune_cluster" {
-  cluster_identifier = "${var.name}-neptune-cluster"
-  engine = "${var.engine}"
-  backup_retention_period = "${var.backup_retention_period}"
-  preferred_backup_window = "${var.backup_window}"
-  skip_final_snapshot       = "${var.skip_final_snapshot}"
+  cluster_identifier                  = "${var.name}-neptune-cluster"
+  engine                              = "${var.engine}"
+  backup_retention_period             = "${var.backup_retention_period}"
+  preferred_backup_window             = "${var.backup_window}"
+  skip_final_snapshot                 = "${var.skip_final_snapshot}"
   iam_database_authentication_enabled = false
-  apply_immediately = true
+  apply_immediately                   = true
 }
 
 resource "aws_neptune_cluster_instance" "neptune_instance" {
@@ -32,8 +32,8 @@ resource "aws_neptune_cluster_instance" "neptune_instance" {
   engine                    = "${var.engine}"
   instance_class            = "${var.instance_class}"
   cluster_identifier        = "${var.name}"
-  parameter_group_name      = "${aws_deb_parameter_group.rds-param-group.name}"
-  apply_immediately = true
+  parameter_group_name      = "${aws_neptune_parameter_group.neptune-param-group.name}"
+  apply_immediately         = true
 
   tags {
     Note = "Deployed by terraform"
--- rds/main.tf
+++ rds/main.tf
@@ -18,7 +18,7 @@ resource "aws_db_instance" "db_instance" {
   name                      = "${var.name}"
   username                  = "${var.username}"
   password                  = "${var.password}"
-  parameter_group_name      = "${aws_deb_parameter_group.rds-param-group.name}"
+  parameter_group_name      = "${aws_db_parameter_group.rds-param-group.name}"
   skip_final_snapshot       = "${var.skip_final_snapshot}"
   db_subnet_group_name      = "${var.subnet_group_name}"
   backup_retention_period   = "${var.backup_retention_period}"
